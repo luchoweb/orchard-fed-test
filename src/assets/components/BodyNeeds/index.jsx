b982d4e22d517a560bebebcc1644baed7258a802
Lucho Web <luchoweb.dev@gmail.com>
2024-08-22 10:33:22 -0500

    path images fixed

--- src/assets/components/BodyNeeds/index.jsx
+++ src/assets/components/BodyNeeds/index.jsx
@@ -1,9 +1,9 @@
-import firstImg from "./image-01.jpg";
-import firstImg2x from "./image-01@2x.jpg";
-import secondImg from "./image-02.jpg";
-import secondImg2x from "./image-02@2x.jpg";
-import thirdImg from "./image-03.jpg";
-import thirdImg2x from "./image-03@2x.jpg";
+import firstImg from "./Image-01.jpg";
+import firstImg2x from "./Image-01@2x.jpg";
+import secondImg from "./Image-02.jpg";
+import secondImg2x from "./Image-02@2x.jpg";
+import thirdImg from "./Image-03.jpg";
+import thirdImg2x from "./Image-03@2x.jpg";
 
 export const images = [
   {
