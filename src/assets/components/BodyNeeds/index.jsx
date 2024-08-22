4024533b6b7d76f9d10c1bff84045edb0b4d054b
Lucho Web <luchoweb.dev@gmail.com>
2024-08-21 22:27:45 -0500

    Body Needs Component Desktop Done

---
+++ src/assets/components/BodyNeeds/index.jsx
@@ -0,0 +1,21 @@
+import firstImg from "./image-01.jpg";
+import firstImg2x from "./image-01@2x.jpg";
+import secondImg from "./image-02.jpg";
+import secondImg2x from "./image-02@2x.jpg";
+import thirdImg from "./image-03.jpg";
+import thirdImg2x from "./image-03@2x.jpg";
+
+export const images = [
+  {
+    x1: firstImg,
+    x2: firstImg2x,
+  },
+  {
+    x1: secondImg,
+    x2: secondImg2x,
+  },
+  {
+    x1: thirdImg,
+    x2: thirdImg2x,
+  },
+];
--- src/components/BodyNeeds/index.jsx
+++ src/components/BodyNeeds/index.jsx
@@ -1,9 +1,30 @@
+import { images } from "../../assets/components/BodyNeeds";
+
 import "../../styles/components/BodyNeeds.scss";
 
 export default function BodyNeeds() {
-  return(
-    <section className="container body-needs">
-      BodyNeeds
+  return (
+    <section className="body-needs container">
+      <div className="body-needs__grid">
+        <div className="grid-col__gallery">
+          {images.map((image, index) => (
+            <picture className="gallery__image" key={`image-${index}`}>
+              <img src={image.x1} srcSet={`${images.x2} 2x`} />
+            </picture>
+          ))}
+        </div>
+
+        <div className="grid-col__content">
+          <h2 className="ft-norma body-needs__title">
+            Answer your body's needs
+          </h2>
+          <hr />
+          <p>The way ingredients are sourced affects the way we nourish our bodies. Author Mark Schatzer believes our body naturally develops an appetite for the foods and nutrients it needs to be healthy, but that artificial flavorings are getting in the way. This can be reversed by focusing on high-quality ingredients and being mindful as your appetite guides you to consume according to your body's needs.</p>
+
+          <h3 className="body-needs__subhead">Be mindful</h3>
+          <p className="body-needs__accent-text">Sourcing local or organic food is a good way to start being more mindful about what you're cooking and eating.</p>
+        </div>
+      </div>
     </section>
   );
 }
