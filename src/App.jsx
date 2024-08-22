b248f1aeec04d095ba78d26dc02dd36f8b81ca1b
Lucho Web <luchoweb.dev@gmail.com>
2024-08-21 21:30:25 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { BodyNeeds } from "./components";
+
+function App() {
+  return (
+    <>
+      <BodyNeeds />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/BodyNeeds/index.jsx
@@ -0,0 +1,9 @@
+import "../../styles/components/BodyNeeds.scss";
+
+export default function BodyNeeds() {
+  return(
+    <section className="container body-needs">
+      BodyNeeds
+    </section>
+  );
+}
